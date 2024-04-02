57f527497028e98daaff39162e9c4faa4e8fefac
WangXinyan940 <wangxy940930@gmail.com>
2024-04-02 09:54:34 +0800

    Update settings.py

--- src/deepdih/settings.py
+++ src/deepdih/settings.py
@@ -1,7 +1,7 @@
 settings = {
     "hbond_repulsion": 0.02, 
     "relax_torsion_bias": 418.4,
-    "ring_atom_rmsd": 4184.0,
+    "ring_atom_rmsd": 20920.0,
     "dihedral_scan_steps": 12,
     "optimization_folds": 3,
     "optimization_steps": 2000
